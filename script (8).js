5bfe762a5365f48dbc033818461fa9dd648a913f
danulokh <khomyndanyil@gmail.com>
2023-05-31 23:34:42 +0300

    practice with function

--- script (8).js
+++ script (8).js
@@ -167,19 +167,81 @@
 //     }
 //     console.log(result)
 
+// const lines = 5;
+// let result = '';
+
+// for (let i = 0; i <= lines; i++) {
+//     for (let j = 0; j < lines - i; j++) {
+//         result += " ";
+//     }
+//     for (let j = 0; j < 2 * i + 1; j++) {
+//         result += "*";
+//     }
+//     result += "\n";
+// }
+
+// console.log(result)
+
 const lines = 5;
 let result = '';
 
-for (let i = 0; i <= lines; i++) {
-    for (let j = 0; j < lines - i; j++) {
-        result += " ";
+for(let i = 0; i <= lines; i++) {
+    for(let j = 0; j < lines - i; j++) {
+        result += ' '
     }
-    for (let j = 0; j < 2 * i + 1; j++) {
-        result += "*";
+    for(let j = 0; j < 2 * i + 1; j++) {
+        result += '*'
     }
-    result += "\n";
+    result += '\n'     
 }
 
 console.log(result)
+
+ /* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+if(numberOfFilms < 10) {
+    console.log("Просмотрено довольно мало фильмов")
+}else if(numberOfFilms > 10 && numberOfFilms < 30) {
+    console.log("Вы классический зритель")
+}else if(numberOfFilms > 30) {
+    console.log("Вы классический зритель")
+} else {
+    console.log("Произошла ошибка")
+}
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+for(let i = 0;  i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', '');
+    if(a.length > 50 || a.length < 1 ) {
+        i--
+    }
+    const b = prompt('На сколько оцените его?', '');
+    personalMovieDB.movies[a] = b;
+}
+
+
+console.log(personalMovieDB);
 
 
